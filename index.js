f679fe4e69494d449413cfd92ff3b1ada8c574df
dayronmiranda <hifamilyg@gmail.com>
2025-08-23 23:16:44 -0400

    v2.2

--- index.js
+++ index.js
@@ -3,31 +3,31 @@
 module.exports = {
     Client: require('./lib/Client'),
     auth: {
-        BaseAuthStrategy: require('./lib/BaseAuthStrategy'),
-        LocalAuth: require('./lib/LocalAuth'),
-        RemoteAuth: require('./lib/RemoteAuth'),
-        NoAuth: require('./lib/NoAuth'),
+        BaseAuthStrategy: require('./lib/authStrategies/BaseAuthStrategy'),
+        LocalAuth: require('./lib/authStrategies/LocalAuth'),
+        RemoteAuth: require('./lib/authStrategies/RemoteAuth'),
+        NoAuth: require('./lib/authStrategies/NoAuth'),
     },
     stores: {
-        Store: require('./lib/Store'),
-        AuthStore: require('./lib/AuthStore')
+        Store: require('./lib/util/Injected/Store'),
+        AuthStore: require('./lib/util/Injected/AuthStore/AuthStore')
     },
     factories: {
-        ChatFactory: require('./lib/ChatFactory'),
-        ContactFactory: require('./lib/ContactFactory'),
+        ChatFactory: require('./lib/factories/ChatFactory'),
+        ContactFactory: require('./lib/factories/ContactFactory'),
     },
     structures: require('./lib/structures'),
     util: {
-        Constants: require('./lib/Constants'),
-        Util: require('./lib/Util'),
+        Constants: require('./lib/util/Constants'),
+        Util: require('./lib/util/Util'),
         Utils: require('./lib/Utils'),
-        Puppeteer: require('./lib/Puppeteer'),
-        InterfaceController: require('./lib/InterfaceController'),
+        Puppeteer: require('./lib/util/Puppeteer'),
+        InterfaceController: require('./lib/util/InterfaceController'),
     },
     webcache: {
-        WebCache: require('./lib/WebCache'),
-        LocalWebCache: require('./lib/LocalWebCache'),
-        RemoteWebCache: require('./lib/RemoteWebCache'),
-        WebCacheFactory: require('./lib/WebCacheFactory'),
+        WebCache: require('./lib/webCache/WebCache'),
+        LocalWebCache: require('./lib/webCache/LocalWebCache'),
+        RemoteWebCache: require('./lib/webCache/RemoteWebCache'),
+        WebCacheFactory: require('./lib/webCache/WebCacheFactory'),
     }
 };
